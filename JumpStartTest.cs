1f5647642c119a30d01966e8f3cf208a96b0947f
Abdallah Ramsing <abdallah.ramsing@sdworx.com>
2023-02-02 12:03:03 +0400

    Adding basic structure

--- JumpStartTest.cs
+++ JumpStartTest.cs
@@ -1,10 +1,37 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
 namespace Visual_Testing;
 
 [TestClass]
 public class JumpStartTest
 {
-    [TestMethod]
-    public void TestMethod1()
+    private IWebDriver _driver = null!;
+
+    [TestInitialize]
+    public void Setup()
     {
+        _driver = ChromeSession();
+    }
+
+    [TestMethod]
+    public void Verify()
+    {
+        _driver.Navigate().GoToUrl("http://localhost:8080/customers");
+    }
+
+    [TestCleanup]
+    public void TearDown()
+    {
+        _driver?.Quit();
+    }
+
+    private WebDriver ChromeSession()
+    {
+        new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+        return new ChromeDriver();
     }
 }
