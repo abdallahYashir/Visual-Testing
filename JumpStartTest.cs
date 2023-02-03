174c1b845b4471b919a4233ae91f104bfb850715
Abdallah Ramsing <abdallah.ramsing@sdworx.com>
2023-02-03 10:52:03 +0400

    Added assert by css value

--- JumpStartTest.cs
+++ JumpStartTest.cs
@@ -33,7 +33,33 @@ public class JumpStartTest
 
         _driver.Navigate().GoToUrl("http://localhost:8080/login");
         Percy.Snapshot(_driver, "Login");
+    }
 
+    // TODO: test method including asserts for acceptance criteria -- just copy existing attributes
+    [TestMethod]
+    public void VerifyFunctionalTest()
+    {
+        _driver.Navigate().GoToUrl("http://localhost:8080/customers");
+        _driver.FindElement(By.CssSelector("a[href='/customers/1/orders']")).Click();
+
+        var tdEl = _driver.FindElement(By.CssSelector("table td"));
+        var tdElWidth = tdEl.GetCssValue("width");
+        var tdElHeight = tdEl.GetCssValue("height");
+        var tdElPadding = tdEl.GetCssValue("padding");
+        var tdElFontSize = tdEl.GetCssValue("font-size");
+        var tdElColor = tdEl.GetCssValue("color");
+        var tdElBackgroundColor = tdEl.GetCssValue("background-color");
+
+        Assert.AreEqual("242.5px", tdElWidth);
+        Assert.AreEqual("20px", tdElHeight);
+        Assert.AreEqual("0px", tdElPadding);
+        Assert.AreEqual("14px", tdElFontSize);
+        Assert.AreEqual("rgba(51, 51, 51, 1)", tdElColor);
+        Assert.AreEqual("rgba(0, 0, 0, 0)", tdElBackgroundColor);
+
+        // think visual testing - how the element UI is going to change - another example with the h4
+
+        // font-size: 18px; margin-top: 10px; margin - bottom: 10px;font-family: inherit; font - weight: 500; line - height: 1.1; color: inherit
     }
 
     [TestCleanup]
